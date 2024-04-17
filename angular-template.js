b4253f164564edb5f28d2da7dff759f34fc2d286
kiforks <ks.kifor@gmail.com>
2024-04-17 19:22:05 +0300

    Fixed angular template config

--- angular-template.js
+++ angular-template.js
@@ -1,3 +1,7 @@
 module.exports = {
+	parser: '@angular-eslint/template-parser',
 	extends: ['angular-strict/template', 'plugin:@angular-eslint/template/process-inline-templates'],
+	rules: {
+		'@angular-eslint/template/attributes-order': 'off',
+	},
 };
